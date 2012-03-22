af5ab5ee485dd2b9850ebe5651b07e45a3e89353
Riccardo Carlesso (Gic) <riccardo_carlesso@dell.com>
2012-03-22 14:30:31 +0000

    cleaned up a bit

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,26 +5,29 @@ $servername  = "puppetmaster.$domain"
 $cluster_ver = '0.9.1'
 
 node riccardonode {
-  #include vanilla
-  include etckeeper
-  include hamachi
-
-  if (defined($description)) {
-    class {'vanilla':
-      machine_description => $description;
-    }
-  } else {
-    include vanilla
-  }
   #include puppetlabs-repo
 
-  # look for and include a class named as per the domain, with
-  # . replaced with _
-  # e.g. for domain example.com, look for a class called example_com
+  # include a class named as per the domain (with'.' replaced with '_'),
+  # if it exists:
+  #  e.g. "example.com" => "domain__example_com"
   $domain_class=regsubst($domain, '\.', '_', 'G')
-  if defined("domain_$domain_class") {
-    class{"domain_$domain_class": }
+  if defined("domain__$domain_class") {
+    class{"domain__$domain_class": }
   }
+}
+
+node production_node inherits riccardonode {
+  
+}
+
+node development_node inherits riccardonode {
+  include etckeeper
+  include hamachi
+  #if (defined('description')) {
+  #  class {'vanilla':
+  #    machine_description => "[riccardonode] $description"
+  #  }
+  #}
 }
 
 class domain_cloud_internal {
@@ -36,30 +39,25 @@ class domain_cloud_internal {
 #}
 
 class development_machines {
-  class { 'puppet':
-    development_machine => true
-  }
+  #class { 'puppet':
+  #  development_machine => true
+  #}
 }
 
 # Home
 
 # Work
-node 'vissani.cloud.internal' inherits riccardonode {
-  $description = 'My beefy workstation at werk'
-  include vanilla
-  include etckeeper
-  include hamachi
+node 'vissani.cloud.internal' inherits production_node  {
+  $description = 'My beefy workstation at werk with 8 beefy processors :)'
+  class {'vanilla': machine_description => $description }
+  #include 'vanilla'
 }
-node 'petomarmitta' inherits riccardonode {
+node 'petomarmitta' inherits development_node {
   $development_machine = 'yes'
-  $description = 'Expendable VM on Vissani'
-  include vanilla
-  include etckeeper
-  include hamachi
+  $description = 'Expendable VM on Vissani built on Virtualbox'
+  class {'vanilla': machine_description => $description }
 }
-node 'cloudia.cloud.internal' inherits riccardonode {
+node 'cloudia.cloud.internal' inherits development_node  {
   $description = 'My VM with hamachi and personal stuff at werk'
-  include vanilla
-  include etckeeper
-  include hamachi
+  class {'vanilla': machine_description => $description }
 }
