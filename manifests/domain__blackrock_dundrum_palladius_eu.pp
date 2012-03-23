32a1f984fe4ccb1da200d8b35b6f6119aa46443f
Riccardo Carlesso (Gic) <riccardo_carlesso@dell.com>
2012-03-23 07:34:13 +0000

    moved vanilla to sauce (ice cream to pasta)

--- manifests/domain__blackrock_dundrum_palladius_eu.pp
+++ manifests/domain__blackrock_dundrum_palladius_eu.pp
@@ -2,15 +2,15 @@
 # file: domain__cloud_internal.pp
 class domain__blackrock_dundrum_palladius_eu {
   # Machines I have at work
-  include vanilla
+  include sauce
 }
 
 # Dundrum host at home in dublin
 
 node 'blackrock' inherits development_node {
   $description = 'My Dell superslow workstation for watching movies'
-  #class {'vanilla': machine_description => $description2 }
-  include 'vanilla'
+  #class {'sauce': machine_description => $description2 }
+  include 'sauce'
   include 'hamachi'
 }
 node 'hansel' inherits development_node {
--- manifests/domain__cloud_internal.pp
+++ manifests/domain__cloud_internal.pp
@@ -2,23 +2,23 @@
 # file: domain__cloud_internal.pp
 class domain__cloud_internal {
   # Machines I have at work
-  include vanilla
+  include sauce
 }
 
 # Work hosts
 
 node 'cloudia.cloud.internal' inherits development_node  {
   $description = 'My VM with hamachi and personal stuff at werk'
-  #class {'vanilla': machine_description => $description1 }
+  #class {'sauce': machine_description => $description1 }
 }
 node 'vissani.cloud.internal' inherits production_node  {
   $description = 'My beefy workstation at werk with 8 beefy processors :)'
-  #class {'vanilla': machine_description => $description2 }
-  #include 'vanilla'
+  #class {'sauce': machine_description => $description2 }
+  #include 'sauce'
 }
 node 'petomarmitta' inherits development_node {
   #$development_machine = 'yes'
   $description = 'Expendable VM on Vissani built on Virtualbox'
   
-  #class {'vanilla': machine_description => $description3 }
+  #class {'sauce': machine_description => $description3 }
 }
