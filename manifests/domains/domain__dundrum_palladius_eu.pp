f5891e245be96d5f4aeb898c7b1a70580534bdba
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-15 21:37:20 +0100

    bugfix

--- manifests/domains/domain__dundrum_palladius_eu.pp
+++ manifests/domains/domain__dundrum_palladius_eu.pp
@@ -30,7 +30,6 @@ node 'blackrock' inherits development_node {
 
 node 'naucrate' inherits development_node {
   $description_naucrate = 'My Debian Squeezy which is f**ed up. I have to create a new Ubuntu machine I guess :)'
-  hamachi::network{'puppet-dundrum':  pass => 'CH4NG3M3!' }
   include sauce
   include hamachi
   include dropbox
@@ -39,22 +38,17 @@ node 'naucrate' inherits development_node {
 }
 
 node 'hansel' inherits development_mac_node {
-  
   $description_hansel = 'My Mac super-client'
   sauce::parsley { 'todo-hansel':
     content => "Find a way to connect to a hamachi list without sharing pass on github :)
      Mydesc: $description_hansel
     "
   }
-  #manazza::add_user { rrunner:  email => "road.runner@acme.com", uid => 505 }
-  #manazza::add_user { rrunner:  email => "road.runner@acme.com", uid => 505 }
-
 }
 
 node 'vbox-hanselmo' inherits development_node {
   $description_hanselmo = 'Hansel VM with Ubuntu 11.10 to test puppet'
   sauce::parsley { 'todo-hanselmo':   content => "Look: $description_hanselmo" }
-  ##manazza::add_user { rrunner:  email => 'road.runner@acme.com', uid => 5005 }
 
   # does it work on a Mac? Dont think so..
   logrotate::file { "puppetric-prova.log": 
