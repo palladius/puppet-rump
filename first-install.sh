61a35483bbe15ddeabb55bc5d315138210d645d1
Riccardo Carlesso <rusko@palladius.it>
2012-06-04 16:46:05 +0200

    Update master

--- first-install.sh
+++ first-install.sh
@@ -3,4 +3,5 @@ git clone git://github.com/palladius/puppet-rump.git /root/rump
 cd /root/rump
 make install # aptget/gem installs (once off)
 make compile # initializes and runs some stuff on external/ricpuppet
-make pull    # updates the data and runs some *OTHER* stuff on external/ricpuppet
+#make pull    # updates the data and runs some *OTHER* stuff on external/ricpuppet
+rump go
