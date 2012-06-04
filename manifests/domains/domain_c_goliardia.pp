c2510a5d82c98930c952890175bbe74e5f4a56d2
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-06-04 15:59:07 +0100

    including sauce with rcarlesso

--- manifests/domains/domain_c_goliardia.pp
+++ manifests/domains/domain_c_goliardia.pp
@@ -5,13 +5,16 @@ class domain__c_goliardia {
   
   # Machines I have in Goliardia
   include sauce
+  class {'sauce': 
+    default_poweruser_name => 'rcarlesso',
+  }
+
   #include etckeeper
-  include hamachi
-  hamachi::network{'puppet-hetzner': pass => 'CH4NG3ME!' }
+  #include hamachi
+  #hamachi::network{'puppet-hetzner': pass => 'CH4NG3ME!' }
 }
 
 node /www.*/ {
-	include sauce
 	#class { 'sauce::tomato':
 	#  machine_description => 'My Web Servers (two)',
 	#  cluster_description => $cluster_description_goliardia,
