af782c8873fb3b630f9466b65369469c7f3be23f
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-27 08:44:19 +0100

    added email

--- bin/create-auto-manifest-for-host.rb
+++ bin/create-auto-manifest-for-host.rb
@@ -4,13 +4,14 @@ require 'rubygems'
 require 'liquid'
 require 'socket'
 
-VER = '0.9.1'
+VER = '0.9.3'
 template_file = './manifests/domains/domain__DOMAIN.pp.liquid'
 hostname = Socket.gethostname
 #unless ARGV.size >= 1
 #  puts "Usage: #{$0} <USERNAME>"
 #  exit 1
 #end
+puts "Usage: #{$0} [<USERNAME>] [<EMAIL>]"
 
 #template_file = ENV['SVNHOME'] + '/templates/liquid/eclipse.project.liquid'
 out_file = template_file.gsub('DOMAIN', hostname).gsub('.liquid$','')
@@ -22,6 +23,7 @@ template = File.open(template_file).read
 results = Liquid::Template.parse(template).render(
    'version'  => VER,
    'fqdn'     => hostname,
+   'email'    => ARGV[1] || ENV['EMAIL'] || 'Email not defined <palladiusbonton@gmail.com>',
    'username' => ARGV[0] || ENV['USER']
 )
 
