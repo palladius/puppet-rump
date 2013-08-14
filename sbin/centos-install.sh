a7fb7f68c4454f77f0fedcd2546c63587a32bcd3
Riccardo Carlesso <rusko@palladius.it>
2013-08-14 12:32:16 +0200

    Update centos-install.sh
    Added rubygems (didnt work otherwise).
    Since puppet 2.7.20 has an error, trying with latest 2.7:
    
    ----
    
    Installing ri documentation for puppet-2.7.20...
    
    
    RDoc failure in lib/puppet/parser/lexer.rb at or around line 253 column 13
    
    Before reporting this, could you check that the file
    you're documenting compiles cleanly--RDoc is not a
    full Ruby parser, and gets confused easily if fed
    invalid programs.
    
    The internal error was:
    
    ERROR:  While executing gem ... (NoMethodError)
        undefined method `name' for #<RubyToken::TkLPAREN:0x7fcc3581a8b8>

--- sbin/centos-install.sh
+++ sbin/centos-install.sh
@@ -1,8 +1,8 @@
-
 # This isntall Riccardo puppet rump on CentOS
-yum install -y git
+yum install -y git rubygems
 gem install rump liquid --no-ri --no-rdoc
-gem install puppet --version 2.7.20                   # doesnt work with Puppet 3 (yet)
+# gem install puppet --version 2.7.20                   # doesnt work with Puppet 3 (yet)
+gem install puppet --version 2.7.22           # doesnt work with Puppet 3 (yet)
 rump clone git://github.com/palladius/puppet-rump
 cd puppet-rump
 rump go
